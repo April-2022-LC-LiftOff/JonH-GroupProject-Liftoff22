142d08cb8d09cbd3f5a2f2b00e41936ae5c094eb
devboyd <devin4boyd@gmail.com>
2022-05-05 18:23:25 -0500

    add landing service file to connect to back end

---
+++ Project/frontend/src/app/landing/landing.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { User } from "../register/registeruser";
+
+@Injectable({
+  providedIn: "root",
+})
+export class RegisterService {
+  constructor(private http: HttpClient) {}
+
+  rootURL = "http://localhost:8080";
+
+  addUser(user: User): Observable<User> {
+    return this.http.post<User>(this.rootURL + "/register", user);
+  }
+}
+
+//work on this later
