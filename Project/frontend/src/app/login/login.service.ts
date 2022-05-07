ec6d63625393e2000aabf639a9b02c6d3130e136
Natasha Matthews <natashamatthews90@gmail.com>
2022-05-07 10:32:42 -0500

    added the login Interface and service

---
+++ Project/frontend/src/app/login/login.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { loginUser } from "../login/loginuser";
+
+@Injectable({
+  providedIn: "root",
+})
+export class LoginService {
+  constructor(private http: HttpClient) {}
+
+  rootURL = "http://localhost:8080";
+
+  addUser(loginuser: loginUser): Observable<loginUser> {
+    return this.http.post<loginUser>(this.rootURL + "/login", loginuser);
+  }
+}
