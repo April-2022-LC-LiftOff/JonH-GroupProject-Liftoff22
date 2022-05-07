817c2c4f2740d2ee08d82e4a71efac8d8d298e19
devboyd <devin4boyd@gmail.com>
2022-05-07 16:52:56 -0500

    remove uneeded User data

--- Project/frontend/src/app/landing/landing.service.ts
+++ Project/frontend/src/app/landing/landing.service.ts
@@ -1,18 +1,16 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { User } from "../register/registeruser";
 
 @Injectable({
   providedIn: "root",
 })
-export class RegisterService {
+export class lll {
   constructor(private http: HttpClient) {}
 
   rootURL = "http://localhost:8080";
 
-  addUser(user: User): Observable<User> {
-    return this.http.post<User>(this.rootURL + "/register", user);
+
   }
 }
 
