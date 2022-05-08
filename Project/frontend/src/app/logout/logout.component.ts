02e1122ceeeeef0523f6b7cf1879ec76c05d289b
Natasha Matthews <natashamatthews90@gmail.com>
2022-05-08 13:27:46 -0500

    rerouting to login after clicking the logout button

--- Project/frontend/src/app/logout/logout.component.ts
+++ Project/frontend/src/app/logout/logout.component.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from "@angular/common/http";
-import { ThrowStmt } from "@angular/compiler";
 import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 import { Observable } from "rxjs";
 import { ConstantsService } from "../constants.service";
 
@@ -10,12 +10,17 @@ import { ConstantsService } from "../constants.service";
   styleUrls: ["./logout.component.css"],
 })
 export class LogoutComponent implements OnInit {
-  constructor(private http: HttpClient, private constants: ConstantsService) {}
+  constructor(
+    private http: HttpClient,
+    private constants: ConstantsService,
+    private router: Router
+  ) {}
 
   ngOnInit() {}
 
   logOut(): void {
     console.log("Logging out.");
+
     const newLocal: Observable<void> = this.http.post<void>(
       this.constants.getRootURL() + "/logout",
       null
@@ -24,8 +29,11 @@ export class LogoutComponent implements OnInit {
     newLocal.subscribe(
       () => {
         console.log("Logged out.");
+        this.router.navigate(["login"]);
       },
-      (error) => {}
+      (error) => {
+        console.error("Error getting user " + JSON.stringify(error));
+      }
     );
   }
 }
