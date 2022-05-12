8ffb3a26126847b1e2ffb8c4a0d797c788fb6e45
devboyd <devin4boyd@gmail.com>
2022-05-12 10:37:41 -0500

    add button functionality to link to add reminder page

--- Project/frontend/src/app/app.module.ts
+++ Project/frontend/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { AppRoutingModule } from "./app-routing.module";
-
 import { AppComponent } from "./app.component";
 import { RegisterComponent } from "./register/register.component";
 import { HttpClientModule } from "@angular/common/http";
@@ -18,7 +17,7 @@ import { LogoutComponent } from "./logout/logout.component";
   declarations: [
   AppComponent, RegisterComponent,
   LoginComponent, PagenotfoundComponent,
-  ReminderComponent, LandingComponent,
+  HomeComponent, ReminderComponent, LandingComponent,
   LogoutComponent
   ],
   imports: [
