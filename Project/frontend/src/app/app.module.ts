a3af34245076e15702ace1972f2bf5e471d7bd67
devboyd <devin4boyd@gmail.com>
2022-05-16 15:20:58 -0500

    add home page declaration

--- Project/frontend/src/app/app.module.ts
+++ Project/frontend/src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { RegisterComponent } from "./register/register.component";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { LoginComponent } from "./login/login.component";
-import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { PagenotfoundComponent} from "./pagenotfound/pagenotfound.component";
 import { HomeComponent } from './home/home.component';
 import { ReminderComponent } from './reminder/reminder.component';
 import { LandingComponent } from "./landing/landing.component";
