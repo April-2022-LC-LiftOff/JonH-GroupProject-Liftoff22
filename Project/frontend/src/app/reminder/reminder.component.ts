9c40b0d37a7c1d6643fca68971615e7e0c98a70c
Natasha Matthews <natashamatthews90@gmail.com>
2022-05-19 18:36:31 -0500

    showing reminder on front end

--- Project/frontend/src/app/reminder/reminder.component.ts
+++ Project/frontend/src/app/reminder/reminder.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import { Router } from "@angular/router";
 import { ReminderService } from "./reminder.service";
 import { Reminder } from "./reminder";
-import { ConstantsService } from '../constants.service';
+import { ConstantsService } from "../constants.service";
+import { ThrowStmt } from "@angular/compiler";
 
 @Component({
-  selector: 'app-reminder',
-  templateUrl: './reminder.component.html',
-  styleUrls: ['./reminder.component.css']
+  selector: "app-reminder",
+  templateUrl: "./reminder.component.html",
+  styleUrls: ["./reminder.component.css"],
 })
-
 export class ReminderComponent implements OnInit {
-    reminder: Reminder = { name: "", description: "", frequency: "", dateCreated: ""};
-    isLoading: boolean = false;
-    frequencies = [
-        {id: 0, name: 'Daily'},
-        {id: 1, name: 'Weekly'},
-        {id: 2, name: 'Monthly'}
-        ];
+  reminder: Reminder = {
+    name: "",
+    description: "",
+    frequency: "",
+    dateCreated: "",
+  };
+  isLoading: boolean = false;
+  frequencies = [
+    { id: 0, name: "Daily" },
+    { id: 1, name: "Weekly" },
+    { id: 2, name: "Monthly" },
+  ];
+
+  reminders: Reminder[] = [];
 
   constructor(
     private reminderService: ReminderService,
@@ -33,6 +40,7 @@ export class ReminderComponent implements OnInit {
       (savedReminder) => {
         console.log(`reminder saved: ${JSON.stringify(savedReminder)}`);
         this.isLoading = false;
+        this.reminders.push(savedReminder);
         this.router.navigate(["reminder"]);
       },
       (e) => {
