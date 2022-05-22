ff670142b713407cc67642e8540e391d1d7dbf84
webbla1 <92279960+webbla1@users.noreply.github.com>
2022-05-22 18:16:07 -0500

    attempted to add backend for push notifications. not working yet

---
+++ Project/src/main/java/com/example/demo/auth/controllers/PushNotificationController.java
@@ -0,0 +1,43 @@
+package com.example.demo.auth.controllers;
+
+import com.example.demo.auth.models.PushNotificationRequest;
+import com.example.demo.auth.models.PushNotificationResponse;
+import com.example.demo.auth.services.PushNotificationService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class PushNotificationController {
+
+    private PushNotificationService pushNotificationService;
+
+    public PushNotificationController(PushNotificationService pushNotificationService) {
+        this.pushNotificationService = pushNotificationService;
+    }
+
+    @PostMapping("/notification/topic")
+    public ResponseEntity sendNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationWithoutData(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @PostMapping("/notification/token")
+    public ResponseEntity sendTokenNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotificationToToken(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @PostMapping("/notification/data")
+    public ResponseEntity sendDataNotification(@RequestBody PushNotificationRequest request) {
+        pushNotificationService.sendPushNotification(request);
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+
+    @GetMapping("/notification")
+    public ResponseEntity sendSampleNotification() {
+        pushNotificationService.sendSamplePushNotification();
+        return new ResponseEntity<>(new PushNotificationResponse(HttpStatus.OK.value(), "Notification has been sent."), HttpStatus.OK);
+    }
+}
---
+++ Project/src/main/java/com/example/demo/auth/firebase/FCMService.java
@@ -0,0 +1,68 @@
+package com.example.demo.auth.firebase;
+
+import com.example.demo.auth.models.PushNotificationRequest;
+import com.google.firebase.messaging.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+@Service
+public class FCMService {
+    private Logger logger = LoggerFactory.getLogger(FCMService.class);
+
+    public void sendMessage(Map<String, String> data, PushNotificationRequest request)
+            throws InterruptedException, ExecutionException {
+        Message message = getPreconfiguredMessageWithData(data, request);
+        String response = sendAndGetResponse(message);
+        logger.info("Sent message with data. Topic: " + request.getTopic() + ", " + response);
+    }
+
+    public void sendMessageWithoutData(PushNotificationRequest request)
+            throws InterruptedException, ExecutionException {
+        Message message = getPreconfiguredMessageWithoutData(request);
+        String response = sendAndGetResponse(message);
+        logger.info("Sent message without data. Topic: " + request.getTopic() + ", " + response);
+    }
+
+    public void sendMessageToToken(PushNotificationRequest request)
+            throws InterruptedException, ExecutionException {
+        Message message = getPreconfiguredMessageToToken(request);
+        String response = sendAndGetResponse(message);
+        logger.info("Sent message to token. Device token: " + request.getToken() + ", " + response);
+    }
+
+    private String sendAndGetResponse(Message message) throws InterruptedException, ExecutionException {
+        return FirebaseMessaging.getInstance().sendAsync(message).get();
+    }
+
+    private ApnsConfig getApnsConfig(String topic) {
+        return ApnsConfig.builder()
+                .setAps(Aps.builder().setCategory(topic).setThreadId(topic).build()).build();
+    }
+
+    private Message getPreconfiguredMessageToToken(PushNotificationRequest request) {
+        return getPreconfiguredMessageBuilder(request).setToken(request.getToken())
+                .build();
+    }
+
+    private Message getPreconfiguredMessageWithoutData(PushNotificationRequest request) {
+        return getPreconfiguredMessageBuilder(request).setTopic(request.getTopic())
+                .build();
+    }
+
+    private Message getPreconfiguredMessageWithData(Map<String, String> data, PushNotificationRequest request) {
+        return getPreconfiguredMessageBuilder(request).putAllData(data).setTopic(request.getTopic())
+                .build();
+    }
+
+    private Message.Builder getPreconfiguredMessageBuilder(PushNotificationRequest request) {
+
+        ApnsConfig apnsConfig = getApnsConfig(request.getTopic());
+        return Message.builder()
+                .setApnsConfig(apnsConfig).setNotification(
+                        new Notification(request.getTitle(), request.getMessage()));
+    }
+}
