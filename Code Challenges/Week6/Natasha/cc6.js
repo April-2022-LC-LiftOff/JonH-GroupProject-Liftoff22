0706316b796acbbc66e3a50b04885d15e63f99e8
Natasha Matthews <natashamatthews90@gmail.com>
2022-05-22 12:33:18 -0500

    added in a couple edge cases

--- Code Challenges/Week6/Natasha/cc6.js
+++ Code Challenges/Week6/Natasha/cc6.js
@@ -1,10 +1,12 @@
 function detectWord(str) {
+  let convertToString = str.toString();
   let arrayFromString = convertToString.split("");
   return arrayFromString.filter(checkCharacterCase).join("");
 }
 
 function checkCharacterCase(char) {
-  if (char.toLowerCase() === char) {
+  let pattern = /[a-z]/g;
+  if (char.match(pattern)) {
     return char;
   }
 }
@@ -14,3 +16,11 @@ console.log(detectWord("UcUNFYGaFYFYGtNUH")); //➞ "cat"
 console.log(detectWord("bEEFGBuFBRrHgUHlNFYaYr")); // ➞ "burglar"
 
 console.log(detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment")); // ➞ "embezzlement"
+
+console.log(detectWord("")); // ➞ ""
+
+console.log(detectWord(1111)); // ➞ ""
+
+console.log(detectWord("!@()848925(()()()")); // ➞ ""
+
+console.log(detectWord("h#$%ell654oworl__*&_d")); // ➞ "helloworld"
